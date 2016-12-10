eb73e8ea5574323654c1a047186bb85ce8a753c1
绿间 <lvjian@meiyaapp.com>
2016-12-10 11:58:38 +0800

    :sparkles: 添加watch

---
+++ config/tasks/clean.js
@@ -0,0 +1,23 @@
+/**
+ * @author: 卓文理
+ * @email : 531840344@qq.com
+ * @desc  : Clean distribution directory
+ */
+'use strict';
+
+const gulp   = require('gulp');
+const del    = require('del');
+const config = require('../config.json');
+
+const cleanTask = function (next) {
+    const module = process.env.MODULE_NAME;
+    const destPath = path.join(module, config.dest);
+
+    const cleanPaths = ['dist', destPath, '.qshell', 'npm-debug*'];
+
+    del(cleanPaths).then(() => next());
+};
+
+gulp.task('clean', cleanTask);
+
+module.exports = cleanTask;
